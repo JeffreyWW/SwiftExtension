fbf8f37266e9585d62ec596d562a538cb171347d
Jeffrey <jeffreyweiok@163.com>
2019-12-26 23:33:03 +0800

    init

---
+++ SwiftExtension/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  SwiftExtension
+//
+//  Created by Jeffrey Wei on 2019/12/26.
+//  Copyright © 2019 JF. All rights reserved.
+//
+
+import UIKit
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+    }
+
+
+    // MARK: UISceneSession Lifecycle
+
+
+
+
+
+}
---
+++ SwiftExtension/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SwiftExtension
+//
+//  Created by Jeffrey Wei on 2019/12/26.
+//  Copyright © 2019 JF. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController
+        // Do any additional setup after loading the view.
+    }
+
+
+}
