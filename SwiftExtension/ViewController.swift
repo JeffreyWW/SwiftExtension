79e8adba733e2b46c524203ab1119e064eda0eb1
Jeffrey <jeffreyweiok@163.com>
2020-01-13 22:13:25 +0800

    empty

--- SwiftExtension/ViewController.swift
+++ SwiftExtension/ViewController.swift
@@ -12,7 +12,40 @@ import SnapKit
 import Moya
 
 class ViewController: UIViewController {
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
+    private lazy var cell: UITableViewCell = {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        return cell
+    }()
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupNavigation()
+    }
+
+    private func setupNavigation() {
+        let item = UIBarButtonItem(title: "test", style: .plain, target: self, action: #selector(clickMe))
+        navigationItem.leftBarButtonItem = item
+    }
+
+    @objc private func clickMe() {
+        print("11")
+
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return cell
     }
 }
