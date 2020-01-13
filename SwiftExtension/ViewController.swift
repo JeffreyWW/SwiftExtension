62e77ea952af4a6ea4157cdcd26c489e87126131
Jeffrey <jeffreyweiok@163.com>
2020-01-13 21:57:47 +0800

    empty

--- SwiftExtension/ViewController.swift
+++ SwiftExtension/ViewController.swift
@@ -12,19 +12,7 @@ import SnapKit
 import Moya
 
 class ViewController: UIViewController {
-//    @PropertyWrapperTest(color: .red, superViewColor: .yellow)
-//    var label: UILabel!
-//    @PropertyWrapperTest(color: .red, superViewColor: .yellow)
-    @PropertyWrapperTest(color: .red)
-    var bgView: UIView!
-    @IBOutlet private var nibLabel: UILabel!
-    @PropertyWrapperTest(color: .blue)
-    @IBOutlet private var button: UIButton!
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        let rx = ProviderManager.stock.rx.request(.saveStock).mapString().subscribe(onSuccess: { string in
-            print(string)
-        })
     }
 }
