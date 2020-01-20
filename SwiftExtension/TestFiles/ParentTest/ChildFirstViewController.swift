d0bf427e3d317987086b30554c011b253d9f8e6c
Jeffrey <jeffreyweiok@163.com>
2020-01-20 09:20:13 +0800

    新增控制器

---
+++ SwiftExtension/TestFiles/ParentTest/ChildFirstViewController.swift
@@ -0,0 +1,9 @@
+//
+// Created by Jeffrey Wei on 2020/1/20.
+// Copyright (c) 2020 Jeffrey Wei. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class ChildFirstViewController:UIViewController {
+}
---
+++ SwiftExtension/TestFiles/ParentTest/ChildSecondViewController.swift
@@ -0,0 +1,10 @@
+//
+// Created by Jeffrey Wei on 2020/1/20.
+// Copyright (c) 2020 Jeffrey Wei. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ChildSecondViewController: UIViewController {
+}
